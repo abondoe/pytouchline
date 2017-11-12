32616e539d70279ff61b20307a81079cf571389c
Aksel Bondø <abo@datatrespons.no>
2017-11-12 12:36:11 +0100

    Bump the version to 0.3

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'touchline',
   packages = ['touchline'],
-  version = '0.2',
+  version = '0.3',
   description = 'A Roth Touchline interface library',
   author = 'Aksel Bondoe',
   author_email = 'aksel.bondoe@gmail.com',
   license='MIT',
   url = 'https://github.com/abondoe/touchline',
-  download_url = 'https://github.com/abondoe/touchline/archive/0.2.tar.gz',
+  download_url = 'https://github.com/abondoe/touchline/archive/0.3.tar.gz',
   keywords = ['Roth', 'Touchline', 'Home Assistant', 'hassio', "Heat pump"],
   classifiers = [
 	'Development Status :: 3 - Alpha',
